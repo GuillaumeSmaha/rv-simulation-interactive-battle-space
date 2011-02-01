8db4c8111f75218111e573f069b6a20dc93536db
Dimitri Bourreau <dimitri.bourreau@gmail.com>
2011-02-01 23:06:18 +0000

    Modifications pour includes de Boost dans Ogre (Windows)

--- cmake/FindOGRE.cmake
+++ cmake/FindOGRE.cmake
@@ -53,6 +53,35 @@ include(PreprocessorUtils)
 findpkg_begin(OGRE)
 
 
+if (WIN32 OR APPLE)
+  set(Boost_USE_STATIC_LIBS TRUE)
+else ()
+  # Statically linking boost to a dynamic Ogre build doesn't work on Linux 64bit
+  set(Boost_USE_STATIC_LIBS ${OGRE_STATIC})
+endif ()
+if (MINGW)
+  # this is probably a bug in CMake: the boost find module tries to look for
+  # boost libraries with name libboost_*, but CMake already prefixes library
+  # search names with "lib". This is the workaround.
+  set(CMAKE_FIND_LIBRARY_PREFIXES ${CMAKE_FIND_LIBRARY_PREFIXES} "")
+endif ()
+set(Boost_ADDITIONAL_VERSIONS "1.42" "1.42.0" "1.41.0" "1.41" "1.40.0" "1.40" "1.39.0" "1.39" "1.38.0" "1.38" "1.37.0" "1.37" )
+# Components that need linking (NB does not include header-only components like bind)
+set(OGRE_BOOST_COMPONENTS thread date_time)
+find_package(Boost COMPONENTS ${OGRE_BOOST_COMPONENTS} QUIET)
+if (NOT Boost_FOUND)
+  # Try again with the other type of libs
+  set(Boost_USE_STATIC_LIBS NOT ${Boost_USE_STATIC_LIBS})
+  find_package(Boost COMPONENTS ${OGRE_BOOST_COMPONENTS} QUIET)
+endif()
+find_package(Boost QUIET)
+
+# Set up referencing of Boost
+include_directories(${Boost_INCLUDE_DIR})
+add_definitions(-DBOOST_ALL_NO_LIB)
+set(OGRE_LIBRARIES ${OGRE_LIBRARIES} ${Boost_LIBRARIES})
+
+
 # Get path, convert backslashes as ${ENV_${var}}
 getenv_path(OGRE_HOME)
 getenv_path(OGRE_SDK)
