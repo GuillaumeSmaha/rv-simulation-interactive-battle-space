1e0cd4b78ba05f71c4bb84998eadee5bfe6f3b80
Guillaume Smaha <guillaume.smaha@gmail.com>
2011-02-06 11:01:32 +0000

    Suppression du SVN du fichier trunk/include/config.h => est généré par CMake

--- include/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CONFIG_H_
-#define _CONFIG_H_
-
-	#define PROJECT_NAME "RV-simulation-bataille"
-	#define PROJECT_VERSION "1.0"
-	#define PROJECT_VERSION_FULL "1.0.0"
-	#define PROJECT_FULLNAME PROJECT_NAME" "PROJECT_VERSION_FULL
-
-#endif
