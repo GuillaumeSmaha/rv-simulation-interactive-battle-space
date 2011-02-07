1ba7321fffcd58af92060d4a412c58245a0dc6e1
Dimitri Bourreau <dimitri.bourreau@gmail.com>
2011-02-07 21:53:10 +0000

    Inutile depuis variables.bat

--- windowsSetPath.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Set Var Env ...
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v BOOST_ROOT /t REG_SZ /d "C:\Users\Administrateur\Downloads\OgreSDK_vc9_v1-7-2\boost_1_44"
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v OGRE_HOME /t REG_SZ /d "C:\Users\Administrateur\Downloads\OgreSDK_vc9_v1-7-2"
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v CEGUI_HOME /t REG_SZ /d "C:\Users\Administrateur\Downloads\CEGUI-SDK-0.7.5-vc9"
-echo Necessite un redemarrage ! ou Fermer et rouvrir la session !
-pause
