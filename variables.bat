a73ef014d9d7379962d41a0f13a0f0e9c6948980
Dimitri Bourreau <dimitri.bourreau@gmail.com>
2011-02-07 16:39:56 +0000

    Réglage automatique des variables d'ENV dans Windows

---
+++ variables.bat
@@ -0,0 +1,9 @@
+@echo off
+set dir=%CD%
+echo Placez ce fichier dans un repertoire contenant les repertoires racines de ogre et cegui (renommez les en 'ogre' et 'cegui')
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v OIS_HOME /t REG_SZ /d "%dir%\ogre"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v OGRE_HOME /t REG_SZ /d "%dir%\ogre"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v CEGUI_ROOT /t REG_SZ /d "%dir%\cegui"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment" /v BOOST_ROOT /t REG_SZ /d "%dir%\ogre\boost_1_42"
+ 
+ 
