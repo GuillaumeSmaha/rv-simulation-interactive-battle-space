98581706807ba4e591694e1f7b41eaa40faa800a
Jérémy Dubois <jrm.dubois@gmail.com>
2011-02-07 17:21:54 +0000

    Mise en place du loader de Meshes

---
+++ include/MeshLoader.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _MESHLOADER_H_
+#define _MESHLOADER_H_
+class MeshLoader
+{
+public:
+	MeshLoader(void);
+	~MeshLoader(void);
+};
+
+
+#endif
---
+++ include/MeshType.h
@@ -0,0 +1,18 @@
+/*!
+ *  \file  MeshType.h
+ *  \brief Ce fichier contient la déclaration des types de mesh
+ */
+#ifndef __MESH_TYPE_H__
+#define __MESH_TYPE_H__
+
+	/*!
+	 *  \brief Liste des différents types de caméra
+	 */
+	typedef enum {
+		SHIP = 0,
+		PLANET = 1,
+		POMPIDOU = 2, //peut etre a modifier
+		ARLEQUIN = 3
+	} EnumTypeMesh;
+
+#endif // __MESH_TYPE_H__
