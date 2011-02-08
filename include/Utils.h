cd8a36ab2aed62348af65ef367db90142c4367b2
Jérémy Dubois <jrm.dubois@gmail.com>
2011-02-08 22:44:04 +0000

    Création d'une classe "utils" qui contient une liste de fonctions souvent utilisées. Pour le moment : -obtenir un entier compris entre [a;b] -obtenir un entier unique tout au long de l'exécution (au moins jusqu'à atteindre MAX_INT) -> ça peut être t'intéresser Pierre pour tes astéroïdes

---
+++ include/Utils.h
@@ -0,0 +1,39 @@
+/**
+ *  @file  Utils.h
+ *  @brief Utilities file
+ */
+#ifndef _UTILS_H_
+#define _UTILS_H_
+#include <math.h>
+#include <time.h>
+/**
+ * @class Utils
+ * @brief A tools class
+ */
+class Utils
+{
+private:
+    /**
+     * Initialize the rand function
+     */
+    static int initialize(void);
+    static int _count;
+public:
+    /**
+    * Returns an int between min and max (inclusive)
+    * @param min lower bound
+    * @param max upper bound
+    * @return An int between min and max (inclusive)
+    */
+	static int randRangeInt(int min, int max);
+	/**
+	 * Returns an unique int
+	 * @return unique int
+	 */
+	static int unique(void);
+};
+
+
+
+
+#endif
---
+++ src/Utils.cpp
@@ -0,0 +1,26 @@
+#include "Utils.h"
+#include <Ogre.h>
+
+
+int Utils::_count = 0;
+int Utils::initialize(void)
+{
+	srand(time(NULL));
+	return 0;
+}
+
+int Utils::randRangeInt(int min, int max)
+{
+    static bool init = false;
+    if(init == false)
+    {
+        init = true;
+        Utils::initialize();
+    }
+	return (int)((double)rand() / ((double)RAND_MAX + 1) * (max-min+1)+min);
+}
+
+int Utils::unique(void)
+{
+    return _count++;
+}
