ab92a7ef1826aeb1029685dbb660c9da1e7a2bfc
Pierre Vittet @pvittet.com <piervit@pvittet.com>
2011-02-12 21:28:53 +0000

    commentaire sur la classe GestShip

--- include/GestShip.h
+++ include/GestShip.h
@@ -1,7 +1,18 @@
 #include <Ogre.h>
 #include "Ship.h"
 
-/***Permet de gérer une liste de vaiseau***/
+/*!
+*  \file GestShip.h
+*  \brief Ce fichier contient la déclaration de la classe GestShip. C'est la classe qui gère l'ensemble des vaiseaux
+*/
+
+/*!
+* \class GestShip
+* \brief Classe gérant l'ensemble des vaiseaux via un std::vecteur.
+* En particulier, on renvoit vers elle les traitements communs à tous les vaiseaux (mise à jour de la position après une frame par exemple.
+*   
+*   
+*/
 class GestShip
 {
     private:
@@ -12,7 +23,8 @@ class GestShip
         ~GestShip();
         void addShip(Ship * ship);
         std::vector<Ship> getAllShips();
-        
+        /*permet de mettre à jour la position de tous les vaiseaux
+        */
         void updateShips();
 
         void deleteAllShips();
