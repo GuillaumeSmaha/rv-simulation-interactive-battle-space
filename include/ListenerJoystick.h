fb511e4b13a1bbf1eb35746f730d27742dc0b996
Jérémy Dubois <jrm.dubois@gmail.com>
2011-03-10 17:35:08 +0000

    Début intégration joyStick

---
+++ include/ListenerJoystick.h
@@ -0,0 +1,73 @@
+#ifndef LISTENERJOYSTICK_H
+#define LISTENERJOYSTICK_H
+
+#include <OISInputManager.h>
+#include <OISJoyStick.h>
+#include "ObjectRoot.h"
+#include "Signal.h"
+#include "Ogre.h"
+class ListenerJoystick : public ObjectRoot
+{
+    private:
+		/*!
+		 *  \brief Gestionnaire d'événements
+		 */
+        OIS::InputManager * inputManager;
+		/*!
+		 *  \brief Capteur d'événements de la souris
+		 */
+        OIS::JoyStick * joystick;
+
+	public:
+        /*!
+		 * \brief Dispatche un signal lorsque la souris bouge Signal(Ogre::Vector3(X, Y, Z)))
+		 */
+        Signal<Ogre::Vector3> signalMouseMoved;
+        /*!
+		 * \brief Dispatche un signal lorsque une touche de la souris est enfoncée Signal(OIS::MouseButtonID)
+		 */
+        Signal<void*> signalMousePressed;
+        /*!
+		 * \brief Dispatche un signal lorsque une touche de la souris est relâchée Signal(OIS::MouseButtonID)
+		 */
+        Signal<void*> signalMouseReleased;
+		/*!
+		 * \brief Constructeur
+		 */
+		ListenerJoystick(OIS::InputManager * inputManager);
+		/*!
+		 * \brief Destructeur
+		 */
+		~ListenerJoystick();
+
+		/*!
+		 * \brief Recupère les événements
+		 */
+		void capture()
+		{
+			this->capture(NULL);
+		}
+		/*!
+		 * \brief Recupère les événements
+		 * \param paramCapture Parametre de capture
+		 */
+		void capture(void * paramCapture);
+
+        bool povMoved(const OIS::JoyStickEvent &e, int pov);
+        bool axisMoved(const OIS::JoyStickEvent &e, int axis );
+        bool sliderMoved(const OIS::JoyStickEvent &e, int sliderID );
+        bool buttonPressed(const OIS::JoyStickEvent &e, int button );
+        bool buttonReleased(const OIS::JoyStickEvent &e, int button );
+        //getter/setter
+
+		/*!
+		 * \brief [Getter] Récupère la valeur de mouse
+		 * \return Le pointeur sur le capteur d'événements de la souris
+		 */
+		OIS::JoyStick * getJoystick()
+		{
+			return this->joystick;
+		}
+};
+
+#endif // LISTENERJOYSTICK_H
---
+++ src/ListenerJoystick.cpp
@@ -0,0 +1,43 @@
+#include "ListenerJoystick.h"
+
+ListenerJoystick::ListenerJoystick(OIS::InputManager * inputManager)
+{
+ 	this->inputManager = inputManager;
+	this->joystick = static_cast<OIS::JoyStick*>(inputManager->createInputObject(OIS::OISJoyStick, true));
+//	this->joystick->setEventCallback(this);
+}
+
+ListenerJoystick::~ListenerJoystick()
+{
+    //dtor
+}
+
+   bool ListenerJoystick::povMoved( const OIS::JoyStickEvent &e, int pov )
+   {
+
+       return true;
+   }
+    bool ListenerJoystick::axisMoved( const OIS::JoyStickEvent &e, int axis )
+
+   {
+
+       return true;
+   }
+    bool ListenerJoystick::sliderMoved( const OIS::JoyStickEvent &e, int sliderID )
+
+   {
+
+       return true;
+   }
+    bool ListenerJoystick::buttonPressed( const OIS::JoyStickEvent &e, int button )
+
+   {
+
+       return true;
+   }
+    bool ListenerJoystick::buttonReleased( const OIS::JoyStickEvent &e, int button )
+
+   {
+
+       return true;
+   }
