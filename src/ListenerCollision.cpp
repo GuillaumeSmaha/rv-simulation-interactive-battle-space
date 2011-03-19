fa17e909e15b6b19c0c0fa62ddf5942269595688
Pierre Vittet @pvittet.com <piervit@pvittet.com>
2011-03-19 20:45:43 +0000

    Enleve le mode debug, ajout d'une petite optimisation de compilateur pour les linuxiens

--- src/ListenerCollision.cpp
+++ src/ListenerCollision.cpp
@@ -5,12 +5,12 @@ ListenerCollision::ListenerCollision(ListenerFrame * listenerFrame)
     //Start Bullet
     mWorld = new OgreBulletDynamics::DynamicsWorld(GestSceneManager::getSceneManager(), Ogre::AxisAlignedBox(Ogre::Vector3 (-1, -1, -1), Ogre::Vector3 (1,  1,  1)), Ogre::Vector3(0,0,0));
     //add Debug info display tool
-    debugDrawer = new OgreBulletCollisions::DebugDrawer();
-    debugDrawer->setDrawWireframe(false);   // we want to see the Bullet containers
-    mWorld->setDebugDrawer(debugDrawer);
-    mWorld->setShowDebugShapes(false);      // enable it if you want to see the Bullet containers
-    Ogre::SceneNode *node_debugDrawer = GestSceneManager::getSceneManager()->getRootSceneNode()->createChildSceneNode("debugDrawer", Ogre::Vector3::ZERO);
-    node_debugDrawer->attachObject(static_cast <Ogre::SimpleRenderable *> (debugDrawer));
+//    debugDrawer = new OgreBulletCollisions::DebugDrawer();
+//    debugDrawer->setDrawWireframe(false);   // we want to see the Bullet containers
+//    mWorld->setDebugDrawer(debugDrawer);
+//    mWorld->setShowDebugShapes(false);      // enable it if you want to see the Bullet containers
+//    Ogre::SceneNode *node_debugDrawer = GestSceneManager::getSceneManager()->getRootSceneNode()->createChildSceneNode("debugDrawer", Ogre::Vector3::ZERO);
+//    node_debugDrawer->attachObject(static_cast <Ogre::SimpleRenderable *> (debugDrawer));
     mWorld->getBulletDynamicsWorld()->clearForces();
 
     listenerFrame->signalFrameStarted.add(&ListenerCollision::updateCollision, this);
